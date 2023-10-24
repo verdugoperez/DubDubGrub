ba8dc2f5c13b5ef08fcf8570bf93912f0cc0afa0
Manuel Alejandro <alejandro>
2023-10-23 18:02:55 -0700

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Administrador on 23/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
