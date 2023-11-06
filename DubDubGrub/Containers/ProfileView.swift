ae9feca3804ac516b8553c1962ab1f56faa3f9f3
Manuel Alejandro <alejandro>
2023-11-06 16:16:22 -0700

    Creando pantalla de perfil

--- DubDubGrub/Containers/ProfileView.swift
+++ DubDubGrub/Containers/ProfileView.swift
@@ -8,8 +8,70 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State private var bioText = ""
+    @State private var totalBioCharacters = 20
+    @Environment(\.colorScheme) var colorScheme
+
     var body: some View {
-        Text("Profile View")
+        VStack {
+            ZStack {
+                RoundedRectangle(cornerRadius: 8)
+                    .frame(height: 100)
+                    .foregroundColor(Color(.secondarySystemBackground))
+                HStack {
+                    Image.defaultAvatar
+                        .resizable()
+                        .scaledToFit()
+                        .clipShape(Circle())
+                        .frame(height: 60)
+                    VStack(alignment: .leading) {
+                        Text("Sean Allen").font(.largeTitle).bold()
+                        Text("YouTuber & Indie Dev")
+                    }
+                    Spacer()
+                }.padding()
+            }
+            .padding(.bottom, 16)
+            HStack {
+                Text("Bio: ") +
+                Text("\(totalBioCharacters)").foregroundColor(totalBioCharacters >= 0 ? .brandPrimary : .red) +
+                Text(" characters remain")
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Label("Check Out", systemImage: "mappin.and.ellipse")
+                        .padding(8)
+                        .foregroundColor(.white)
+                        .background(Color.pink)
+                        .cornerRadius(8)
+                        .bold()
+                }
+            }
+            TextEditor(text: $bioText)
+                .frame(height: 80)
+                .padding(10)
+                .background(RoundedRectangle(cornerRadius: 8)
+                    .stroke(colorScheme == .dark ? Color.white : Color.black, lineWidth: 2))
+                .cornerRadius(8)
+                .onChange(of: bioText) { newText in
+                    totalBioCharacters -= 1
+                }
+            
+            Spacer()
+            Button {
+                
+            } label: {
+                Text("Save Profile")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 40)
+                    .foregroundColor(.white)
+                    .background(Color.brandPrimary)
+                    .cornerRadius(8)
+                    .bold()
+                    .padding(16)
+            }
+        }.padding()
     }
 }
 
